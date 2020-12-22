cb6609b60bcd711ba6e46fab36323d6da7496b68
Jessica Garson <jgarson@twitter.com>
2020-12-22 14:53:05 -0500

    adding twitter google sheets

--- twitter_google_sheets.py
+++ twitter_google_sheets.py
@@ -7,24 +7,25 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 def connect_to_twitter():
-    bearer_token = os.environ.get('BEARER_TOKEN')
+    bearer_token = os.environ.get("BEARER_TOKEN")
     return {"Authorization": "Bearer {}".format(bearer_token)}
 
 
 def make_request(headers):
     url = "https://api.twitter.com/2/tweets"
-    params = "tweet.fields=author_id,created_at,lang&ids=21,1293593516040269825,1334542969530183683"
+    params = {
+        "tweet.fields": "author_id,created_at,lang",
+        "ids": "21,1293593516040269825,1334542969530183683",
+    }
     return requests.request("GET", url, headers=headers, params=params).json()
 
 
 def make_df(response):
-    return pd.DataFrame(response['data'])
+    return pd.DataFrame(response["data"])
 
 
 def authenticate_to_google():
-    scope = [
-        "https://spreadsheets.google.com/feeds"
-    ]
+    scope = ["https://spreadsheets.google.com/feeds"]
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         "/path/to/your/file.json", scope
     )
@@ -37,9 +38,9 @@ def main():
     df = make_df(response)
     credentials = authenticate_to_google()
     gc = gspread.authorize(credentials)
-    workbook = gc.open_by_key('spreadsheet_id')
-    sheet = workbook.worksheet('Sheet1')
-    sheet.update('A1', [df.columns.values.tolist()] + df.values.tolist())
+    workbook = gc.open_by_key("spreadsheet_id")
+    sheet = workbook.worksheet("Sheet1")
+    sheet.update("A1", [df.columns.values.tolist()] + df.values.tolist())
 
 
 if __name__ == "__main__":
